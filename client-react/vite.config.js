687a47b9de71c1b74ddaf87eb01e97e1ba8e4168
Dipec001 <dpecchukwu@gmail.com>
2025-05-15 16:00:30 +0100

    `feat: update .air.toml, main.go, package.json, App.jsx, and vite.config.js for polling and API changes`

--- client-react/vite.config.js
+++ client-react/vite.config.js
@@ -13,18 +13,21 @@ export default defineConfig({
     proxy: {
       '/api/golang': {
         // target: 'http://localhost:8080',
-        target: 'http://api-golang-container:8080',
+        target: 'http://api-golang:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/golang/, ''),
         secure: false,
       },
       '/api/node': {
         // target: 'http://localhost:3000',
-        target: 'http://api-node-container:3000',
+        target: 'http://api-node:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/node/, ''),
         secure: false,
       },
     },
+    watch: {
+      usePolling: true,
+    }
   },
 });
